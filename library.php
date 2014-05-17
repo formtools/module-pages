03d9b12b7d3188f1891eba743797abd64950d5c5
Ben Keen <ben.keen@gmail.com>
2014-05-17 15:29:30 -0700

    1.0.0-beta-20090114

--- library.php
+++ library.php
@@ -176,3 +176,74 @@ function pg_update_page($page_id, $info)
 
 	return array(true, $LANG["notify_page_updated"]);
 }
+
+
+/**
+ * The installation script for the Pages module. This creates the module_pages
+ * database table.
+ */
+function pages__install($module_id)
+{
+	global $g_table_prefix, $LANG;
+
+	// our create table query
+	$query = "
+		CREATE TABLE {$g_table_prefix}module_pages (
+	    page_id mediumint(8) unsigned NOT NULL auto_increment,
+	    page_name varchar(50) NOT NULL,
+	    heading varchar(255) default NULL,
+	    content text,
+	    PRIMARY KEY  (page_id)
+	   ) ENGINE=InnoDB
+  ";
+
+	$result = mysql_query($query);
+
+	$message = "";
+	if ($result)
+		$success = true;
+	else
+	{
+		$success = false;
+
+		// get the error message
+		$mysql_error = mysql_error();
+		$message =  ft_eval_smarty_string($LANG["pages"]["notify_problem_installing"], array("error" => $mysql_error));
+	}
+
+	return array($success, $message);
+}
+
+
+/**
+ * The uninstallation script for the Pages module. This basically does a little clean up
+ * on the database to ensure it doesn't leave any footprints. Namely:
+ *   - the module_pages table is removed
+ *   - any references in client or admin menus to any Pages are removed
+ *   - if the default login page for any user account was a Page, it attempts to reset it to
+ *     a likely login page (the Forms page for both).
+ *
+ * The message returned by the script informs the user the module has been uninstalled, and warns them
+ * that any references to any of the Pages in the user accounts has been removed.
+ *
+ * @return array [0] T/F, [1] success message
+ */
+function pages__uninstall($module_id)
+{
+	global $g_table_prefix, $LANG;
+
+	$pages = mysql_query("SELECT page_id FROM {$g_table_prefix}module_pages");
+	while ($row = mysql_fetch_assoc($pages))
+	{
+	  $page_id = $row["page_id"];
+	  mysql_query("DELETE FROM {$g_table_prefix}menu_items WHERE page_identifier = 'page_{$page_id}");
+	}
+
+	// delete the Pages module table
+	$result = mysql_query("DROP TABLE {$g_table_prefix}module_pages");
+
+	// update sessions in case a Page was in the administrator's account menu
+	ft_cache_account_menu($account_id = $_SESSION["ft"]["account"]["account_id"]);
+
+	return array(true, $LANG["pages"]["notify_module_uninstalled"]);
+}
--- module.php
+++ module.php
@@ -9,8 +9,8 @@
 $MODULE["author"]          = "Encore Web Studios";
 $MODULE["author_email"]    = "formtools@encorewebstudios.com";
 $MODULE["author_link"]     = "http://www.encorewebstudios.com";
-$MODULE["version"]         = "1.0.0-beta-20081226";
-$MODULE["date"]            = "2008-12-26";
+$MODULE["version"]         = "1.0.0-beta-20090114";
+$MODULE["date"]            = "2009-01-14";
 $MODULE["origin_language"] = "en_us";
 $MODULE["supports_ft_versions"] = "2.0.0";
 
